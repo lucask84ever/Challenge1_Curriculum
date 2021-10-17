69c49aeb646342a78a69348132c22f5773ae6f22
lucas.silva <lucas.silva@ZUP-7085.local>
2021-10-17 10:16:11 -0300

    Initial commit

---
+++ Challenge1_Curriculum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge1_Curriculum
+//
+//  Created by lucas.silva on 17/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
