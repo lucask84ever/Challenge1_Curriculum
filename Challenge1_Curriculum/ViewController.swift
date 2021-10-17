923a09562ae54ddb04b7ca8ce2e7e2164376947f
lucask84ever <fox_krb@yahoo.com.br>
2021-10-17 15:09:54 -0300

    Final setup

--- Challenge1_Curriculum/ViewController.swift
+++ Challenge1_Curriculum/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var curriculumTableView: UITableView!
+    
+    let movies: [String] = ["Homem de ferro 2 - 2008",
+                            "Os Vingadores - 2012",
+                            "Capitão América 2: Soldado Invernal - 2014",
+                            "Vingadores: Era de Ultron - 2015",
+                            "Capitão América 3: Guerra Civil - 2016",
+                            "Vingadores: Guerra Infinita - 2018",
+                            "Capitã Marvel - 2019",
+                            "Vingadores: Ultimato - 2019",
+                            "Viúva Negra - 2021"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        curriculumTableView.delegate   = self
+        curriculumTableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = movies[indexPath.row]
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate { }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Challenge1_Curriculum
+
+## Desafio do Curriculum!
+
+Vamos apresentar em uma tableview todos os filmes da Marvel que contam com a participação da Scarlett Johansson.
+
+## Requisitos
++ Xcode 12
+
+## Desafios: 
+- Criar uma classe Filme com atributo de Nome, ano de estreia e imagem.
+- Criar uma célula customizada que apresente os dados do filme com a imagem.
+- Remover as células não utilizadas.
+
+## Objetivos:
+ - Entender o funcionamento de tableviews
+ - Criar células customizadas
+ - Utilizar assets de dentro do projeto
